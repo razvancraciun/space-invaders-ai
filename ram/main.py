365546666f576694dbec6d5c420fe56cbdf06cc3
root <razvancraciun9851@gmail.com>
2020-01-02 17:44:36 +0000

    Modified some params

--- ram/main.py
+++ ram/main.py
@@ -3,27 +3,19 @@ import gym
 import json
 import matplotlib
 import numpy as np
-import subprocess as sh
-
 
 CONFIG_FILE = 'config.json'
 
 def log(scores):
-	f = open('log.txt', 'w')
+	f = open('/content/drive/My Drive/save2/log.txt', 'w')
 	f.write(str(scores))
 	f.close()
 
 
-def commit():
-	sh.run('git add .', check=True, shell=True)
-	sh.run('git commit -m "Training..."', check=True, shell=True)
-	sh.run('git push origin master', check=True, shell=True)
-
-
 def main():
 	env = gym.make('SpaceInvaders-ram-v0')
-	games = 1000
-	save_interval = 1
+	games = 10000
+	save_interval = 100
 	agent = Agent(CONFIG_FILE, input_shape=128, n_actions=6)
 	scores = []
 
@@ -37,9 +29,9 @@ def main():
 			to_state, reward, done, info = env.step(action)
 			score += reward
 			if done and info['ale.lives'] == 0:
-				reward = -1e5
+				reward = -1e7
 			elif info['ale.lives'] < current_lives:
-				reward = -100
+				reward = -1e6
 				current_lives = info['ale.lives']
 			elif reward == 0:
 				reward = -1
@@ -51,7 +43,6 @@ def main():
 		avg_score = np.mean(scores[max(0, i-10): (i+1)])
 		if i % save_interval == 0 and i != 0:
 			agent.save(i)
-			commit()
 			log(scores)
 		print(f'episode:{i} score:{round(score,2)} avg_score:{round(avg_score,2)}')
 
